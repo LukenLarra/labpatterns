8d24e61a71723eae96b64ba6a6ece6d5d1126eeb
unaitor13 <umatas002@ikasle.ehu.eus>
2024-10-24 16:54:57 +0200

    implements added

--- src/adapter2/Covid19PacientTableModelAdapter.java
+++ src/adapter2/Covid19PacientTableModelAdapter.java
@@ -1,18 +1,20 @@
 package adapter2;
 
-import javax.swing.table.AbstractTableModel;
-
 import domain.Covid19Pacient;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
 
-public class Covid19PacientTableModelAdapter extends AbstractTableModel {
+public class Covid19PacientTableModelAdapter extends AbstractTableModel implements TableModel {
 	  protected Covid19Pacient pacient;
 	  protected String[] columnNames =
 	    new String[] {"Symptom", "Weight" };
+	  
 
 	  public Covid19PacientTableModelAdapter(Covid19Pacient p) {
 	    this.pacient=p;
 	  }
 
+	  @Override
 	  public int getColumnCount() {
 	    // Challenge!
 		 return 1;
