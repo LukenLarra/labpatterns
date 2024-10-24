d98163645906df362d6b7d1723a9366cc3977637
LukenLarra <llarranaga024@ikasle.ehu.eus>
2024-10-24 16:46:09 +0200

    Refactorizar código para agregar nuevos pacientes y mostrar su tabla de síntomas

--- src/adapter2/Main.java
+++ src/adapter2/Main.java
@@ -10,13 +10,22 @@ public class Main {
 		pacient.addSymptomByName("disnea", 2);
 		pacient.addSymptomByName("cefalea", 1);
 		pacient.addSymptomByName("astenia", 3);
+
+		Covid19Pacient pacient2=new Covid19Pacient("aitor", 35);
+
+		pacient2.addSymptomByName("fiebre", 2);
+		pacient2.addSymptomByName("vomitos", 1);
+		pacient2.addSymptomByName("nauseas", 3);
 		
 		ShowPacientTableGUI gui=new ShowPacientTableGUI(pacient);
+		ShowPacientTableGUI gui2=new ShowPacientTableGUI(pacient2);
 		gui.setPreferredSize(
 			      new java.awt.Dimension(300, 200));
 		gui.setVisible(true);
-
-
+		
+		gui2.setPreferredSize(
+			      new java.awt.Dimension(300, 200));
+		gui2.setVisible(true);
 	}
 
 }
