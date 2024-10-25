54a62423c0249dc02853db922c77712eb8ba3e21
LukenLarra <llarranaga024@ikasle.ehu.eus>
2024-10-25 15:11:31 +0200

    partially implemented adapter2

--- src/adapter2/Covid19PacientTableModelAdapter.java
+++ src/adapter2/Covid19PacientTableModelAdapter.java
@@ -1,37 +1,44 @@
 package adapter2;
 
 import domain.Covid19Pacient;
+import domain.Symptom;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.table.AbstractTableModel;
-import javax.swing.table.TableModel;
 
-public class Covid19PacientTableModelAdapter extends AbstractTableModel implements TableModel {
-	  protected Covid19Pacient pacient;
-	  protected String[] columnNames =
-	    new String[] {"Symptom", "Weight" };
-	  
+public class Covid19PacientTableModelAdapter extends AbstractTableModel{
+	private Covid19Pacient pacient;
+	private String[] columnNames = new String[] {"Symptom", "Weight" };
+	private List<Symptom> symptoms;
 
-	  public Covid19PacientTableModelAdapter(Covid19Pacient p) {
-	    this.pacient=p;
-	  }
-
-	  @Override
-	  public int getColumnCount() {
-	    // Challenge!
-		 return 1;
-	  }
-
-	  public String getColumnName(int i) {
-	    // Challenge!
-		  return "Column name 1";
-	  }
-
-	  public int getRowCount() {
-	    // Challenge!
-		  return 1;
-	  }
-
-	  public Object getValueAt(int row, int col) {
-	    // Challenge!
-		  return "value";
-	  }
+	public Covid19PacientTableModelAdapter(Covid19Pacient p) {
+		this.pacient=p;
+        this.symptoms = new ArrayList<>(p.getSymptoms()); 
 	}
+
+	@Override
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    @Override
+    public String getColumnName(int i) {
+        return columnNames[i];
+    }
+
+    @Override
+    public int getRowCount() {
+        return symptoms.size(); // Número de síntomas en el paciente
+    }
+
+    @Override
+    public Object getValueAt(int row, int col) {
+        Symptom symptom = symptoms.get(row);
+		if (col==0){
+			return symptom.getName(); 
+		}else if (col==1) {
+			return pacient.getWeight(symptom);
+		}
+		return null;
+    }
+}
--- src/adapter2/Main.java
+++ src/adapter2/Main.java
@@ -5,26 +5,23 @@ import domain.Covid19Pacient;
 public class Main {
 
 	public static void main(String[] args) {
-		Covid19Pacient pacient=new Covid19Pacient("aitor", 35);
-		
-		pacient.addSymptomByName("disnea", 2);
-		pacient.addSymptomByName("cefalea", 1);
-		pacient.addSymptomByName("astenia", 3);
+		Covid19Pacient pacient1=new Covid19Pacient("aitor", 35);
+		pacient1.addSymptomByName("disnea", 2);
+		pacient1.addSymptomByName("cefalea", 1);
+		pacient1.addSymptomByName("astenia", 3);
 
-		Covid19Pacient pacient2=new Covid19Pacient("aitor", 35);
-
+		Covid19Pacient pacient2=new Covid19Pacient("paco", 20);
 		pacient2.addSymptomByName("fiebre", 2);
 		pacient2.addSymptomByName("vomitos", 1);
 		pacient2.addSymptomByName("nauseas", 3);
 		
-		ShowPacientTableGUI gui=new ShowPacientTableGUI(pacient);
+		ShowPacientTableGUI gui=new ShowPacientTableGUI(pacient1);
 		ShowPacientTableGUI gui2=new ShowPacientTableGUI(pacient2);
-		gui.setPreferredSize(
-			      new java.awt.Dimension(300, 200));
+
+		gui.setPreferredSize(new java.awt.Dimension(500, 500));
 		gui.setVisible(true);
 		
-		gui2.setPreferredSize(
-			      new java.awt.Dimension(300, 200));
+		gui2.setPreferredSize(new java.awt.Dimension(500, 400));
 		gui2.setVisible(true);
 	}
 
